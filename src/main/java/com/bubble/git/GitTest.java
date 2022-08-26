a24203c5b005db376aa5e54c0847507cf89ebb3c
bubble997 <111424038+bubble997@users.noreply.github.com>
2022-08-26 17:16:17 +0800

    pull commit

--- src/main/java/com/bubble/git/GitTest.java
+++ src/main/java/com/bubble/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("master test 冲突合并：两个分支都做了修改进行合并就会代码冲突");
         System.out.println("hello hot-fix2 冲突合并");
         System.out.println("push test!!");
+        System.out.println("pull test by github ^.^");
+        
     }
 }
